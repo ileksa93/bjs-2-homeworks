47eb0aedfe7ffa08195b76b209ee31cac6e6c8bd
ileksa <ileksababy@gmail.com>
2023-02-09 01:29:55 +0300

    fwegwgwgh

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,12 +1,5 @@
 function compareArrays(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++);
-    
-    if (arr1[i] !== arr2[i]){
- 
-    return false;
-    } else{
-    return true;
-    }
+    return arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
 }
  
 }
