2920ad684fb9595bfe81a15751f582b49401ff4e
ileksa <ileksababy@gmail.com>
2023-02-09 01:31:16 +0300

    fegrhtjhterw

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,7 +2,6 @@ function compareArrays(arr1, arr2) {
     return arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
 }
  
-}
 
 function getUsersNamesInAgeRange(users, gender) {
   
