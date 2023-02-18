c6c917a60643259d7461e5dbca445533645d1854
ileksa <ileksababy@gmail.com>
2023-02-18 11:40:52 +0300

    dqwewfgetf

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,6 +1,28 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    function cachingDecoratorNew(func) {
+        let cache = [];
+      
+        function wrapper(...args) {
+          const hash = md5(JSON.stringify(args));
+          let objectInCache = cache.find((item) => item.hash === hash);
+          if (objectInCache) {
+            console.log("Из кеша: " + objectInCache.value);
+            return "Из кеша: " + objectInCache.value;
+          }
+      
+          let result = func(...args);
+          cache.push({ hash, value: result });
+          if (cache.length > 5) {
+            cache.shift();
+          }
+          console.log("Вычисляем: " + result);
+          return "Вычисляем: " + result;
+        }
+      
+        return wrapper;
+      }
+      
 }
 
 //Задача № 2
